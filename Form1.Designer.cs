169e24aede2404aa5dfc3d4d2674ab066e1d5838
User <User@DESKTOP-3VB9B1L>
2022-05-18 01:20:35 +0500

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    partial class PlayGame
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PlayGame));
+            this.playButton = new System.Windows.Forms.Label();
+            this.nameOfGame = new System.Windows.Forms.Label();
+            this.passwordBox = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // playButton
+            // 
+            this.playButton.Font = new System.Drawing.Font("Segoe UI", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.playButton.ForeColor = System.Drawing.Color.Moccasin;
+            this.playButton.Image = ((System.Drawing.Image)(resources.GetObject("playButton.Image")));
+            this.playButton.Location = new System.Drawing.Point(301, 460);
+            this.playButton.Name = "playButton";
+            this.playButton.Size = new System.Drawing.Size(424, 100);
+            this.playButton.TabIndex = 0;
+            this.playButton.Text = "  PLAY GAME";
+            this.playButton.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.playButton.MouseClick += new System.Windows.Forms.MouseEventHandler(this.playButton_MouseClick);
+            // 
+            // nameOfGame
+            // 
+            this.nameOfGame.Font = new System.Drawing.Font("Segoe UI", 28F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.nameOfGame.ForeColor = System.Drawing.Color.Moccasin;
+            this.nameOfGame.Image = ((System.Drawing.Image)(resources.GetObject("nameOfGame.Image")));
+            this.nameOfGame.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.nameOfGame.Location = new System.Drawing.Point(236, -4);
+            this.nameOfGame.Name = "nameOfGame";
+            this.nameOfGame.Size = new System.Drawing.Size(550, 164);
+            this.nameOfGame.TabIndex = 1;
+            this.nameOfGame.Text = "  Cats and Scientists";
+            this.nameOfGame.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            // 
+            // passwordBox
+            // 
+            this.passwordBox.BackColor = System.Drawing.SystemColors.Control;
+            this.passwordBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.passwordBox.Font = new System.Drawing.Font("Segoe UI", 22F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.passwordBox.Location = new System.Drawing.Point(389, 571);
+            this.passwordBox.Multiline = true;
+            this.passwordBox.Name = "passwordBox";
+            this.passwordBox.Size = new System.Drawing.Size(266, 63);
+            this.passwordBox.TabIndex = 2;
+            // 
+            // PlayGame
+            // 
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(1026, 836);
+            this.Controls.Add(this.passwordBox);
+            this.Controls.Add(this.nameOfGame);
+            this.Controls.Add(this.playButton);
+            this.Name = "PlayGame";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+
+        #endregion
+
+        private System.Windows.Forms.Label playButton;
+        private System.Windows.Forms.Label nameOfGame;
+        private System.Windows.Forms.TextBox passwordBox;
+        private Timer timer1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Net;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+
+
+namespace WindowsFormsApp1
+{
+    
+    public partial class PlayGame : Form
+    {
+        public string[,] quizes = { 
+        {"Какая наука изучает грибы?", "Микология", "Ихтиология", "Анатомия", "Цитология", "Микология"}, 
+        {"Единица измерения мощности", "Ампер", "Джоуль", "Ватт", "Ом", "Ватт"},
+        {"Какой город объявлен официальной родиной русского Деда Мороза?", "Малая Вишера", "Великий Устюг", "Вышний Волочек", "Нижний Новгород", "Великий Устюг"},
+        {"(x-1)^3=8, x=?", "1", "0", "3", "4", "3"},
+        {"Кому было адресовано письмо Евгений Онегина?", "Татьяне", "Ольге", "Ленскому", "Жене", "Татьяне"},
+        {"В каком штате США находится Силиконовая долина?", "Флорида", "Калифорния", "Джорджия", "Нью-Джерси", "Калифорния"},
+        {"Элементарная единица наследственности", "Клетка", "Атом", "Ген", "РНК", "Ген"},
+        {"В каком городе находится пирамида Хеопса?", "Гиза", "Каир", "Александрия", "Хелуан", "Гиза"},
+        {"Страна, крупнейший экспортёр чая", "Китай", "Индия", "Россия", "Африка", "Индия"},
+        {"Что является результатом дифференцирования функции?", "Интеграл", "Дифференциал", "Степень числа", "Производная", "Производная"},
+        {"Столица Индонезии", "Джакарта", "Дели", "Мехико", "Богота", "Джакарта"},
+        {"Кто является главным героем романа Преступление и наказание?", "Раскольников", "Болконский", "Ростов", "Чацкий", "Раскольников"},
+        {"Последний российский император", "Александр", "Николай", "Петр", "Павел", "Николай"},
+        {"Кто проводил реформы в 1906 году?", "Петр", "Столыпин", "Екатерина", "Николай", "Столыпин"},
+        {"В какое озеро упал метеорит в 2013 году?", "Чебаркуль", "Тургояк", "Увильды", "Сугояк", "Чебаркуль"}
+        };
+        public Button[] controllers;
+        public int catHealth = 100;
+        public int scientistHealth = 100;
+        public PlayGame()
+        {
+            InitializeComponent();
+
+            timer1.Interval = 10;
+            timer1.Tick += Timer1_Tick;
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+
+        }
+        //шляпа выше +- рабочая, но желательно переделать
+        private void playButton_MouseClick(object sender, MouseEventArgs e)
+        {
+                CreateCatGame("Cat sprite.png");
+                controllers = CreateGame();
+                var catHealthBar = CreateHealth(new Point(430, 600));
+                catHealthBar.Value = 100;
+                Controls.Add(catHealthBar);
+                var scientistHealthBar = CreateHealth(new Point(430, 200));
+                scientistHealthBar.Value = 100;
+                Controls.Add(scientistHealthBar);
+                var question = CreateLabel(new Size(530, 90), new Point(258, 264));
+                Controls.Add(question);
+                Random rnd = new Random();
+                //Думай как делать, ДУУУУУМААААЙ
+                int quiz = rnd.Next(0, 14);
+                question.Text = quizes[quiz, 0];
+                controllers[0].Text = quizes[quiz, 1];
+                controllers[1].Text = quizes[quiz, 2];
+                controllers[2].Text = quizes[quiz, 3];
+                controllers[3].Text = quizes[quiz, 4];
+
+                controllers[0].Click += (sender, args) => doQuiz(1, controllers, quizes, quiz, catHealthBar, scientistHealthBar, question);
+                controllers[1].Click += (sender, args) => doQuiz(2, controllers, quizes, quiz, catHealthBar, scientistHealthBar, question);
+                controllers[2].Click += (sender, args) => doQuiz(3, controllers, quizes, quiz, catHealthBar, scientistHealthBar, question);
+                controllers[3].Click += (sender, args) => doQuiz(4, controllers, quizes, quiz, catHealthBar, scientistHealthBar, question);
+        }
+
+
+        public void doQuiz(int i, Button[] controllers, string[,] quizes, int quiz,
+            ProgressBar catHealthBar, ProgressBar scientistHealthBar, Label question)
+        {
+            if(catHealthBar.Value == 0 || scientistHealthBar.Value == 0)
+            {
+                Controls.Clear();
+                var gameOver = new PictureBox()
+                {
+                    Size = new Size(512, 128),
+                    Location = new Point(275, 340)
+                };
+                System.IO.FileStream fs = new System.IO.FileStream(@"..\..\..\Sprites\GameOver.png", System.IO.FileMode.Open);
+                System.Drawing.Image img = System.Drawing.Image.FromStream(fs);
+                fs.Close();
+                gameOver.Image = img;
+                gameOver.BackColor = Color.Transparent;
+                Controls.Add(gameOver);
+            }
+            if (controllers[i - 1].Text == quizes[quiz, 5])
+            {
+                scientistHealth = scientistHealth - 10;
+                scientistHealthBar.Value = scientistHealth;
+            }
+            //А 7 строк ниже отвечают за урон противнику, тоже тема рабочая
+            Random rnd = new Random();
+            int a = rnd.Next(0, 1);
+            if (10 * a > 0)
+            {
+                catHealth = catHealth - 10;
+                catHealthBar.Value = catHealth;
+            }
+            //Хуйню ниже желательно переделать. проблема где-то в ней
+            quiz = rnd.Next(0, 14);
+            question.Text = quizes[quiz, 0];
+            controllers[0].Text = quizes[quiz, 1];
+            controllers[1].Text = quizes[quiz, 2];
+            controllers[2].Text = quizes[quiz, 3];
+            controllers[3].Text = quizes[quiz, 4];
+
+
+            controllers[0].Click += (sender, args) => doQuiz(1, controllers, quizes, quiz, catHealthBar, scientistHealthBar, question);
+            controllers[1].Click += (sender, args) => doQuiz(2, controllers, quizes, quiz, catHealthBar, scientistHealthBar, question);
+            controllers[2].Click += (sender, args) => doQuiz(3, controllers, quizes, quiz, catHealthBar, scientistHealthBar, question);
+            controllers[3].Click += (sender, args) => doQuiz(4, controllers, quizes, quiz, catHealthBar, scientistHealthBar, question);
+        }
+
+        public void CreateCatGame(String catSprite)
+        {
+            Controls.Clear();
+            Controls.Add(createHero(new Point(415, 635), catSprite, "Catgirl"));
+            Controls.Add(createHero(new Point(415, 0), "Angry scientist1.png", "Angry Scientist"));
+        }
+        
+        public PictureBox createHero(Point location, String sprite, string name)
+        {
+            PictureBox hero = new PictureBox()
+            {
+                Location = location,
+                Size = new Size(200, 200)
+            };
+            System.IO.FileStream fs = new System.IO.FileStream(@"..\..\..\Sprites\" + sprite, System.IO.FileMode.Open);
+            System.Drawing.Image img = System.Drawing.Image.FromStream(fs);
+            fs.Close();
+            hero.Image = img;
+            hero.BackColor = Color.Transparent;
+            return hero;
+        }
+
+        public Button[] CreateGame()
+        {
+            
+            var answer1 = CreateButton(new Size(260, 65), new Point(258, 376));
+            Controls.Add(answer1);
+            var answer2 = CreateButton(new Size(260, 65), new Point(528, 376));
+            Controls.Add(answer2);
+            var answer3 = CreateButton(new Size(260, 65), new Point(528, 460));
+            Controls.Add(answer3);
+            var answer4 = CreateButton(new Size(260, 65), new Point(258, 460));
+            Controls.Add(answer4);
+            var allControls = new[] {answer1, answer2, answer3, answer4};
+            return allControls;
+        }
+
+        public Button CreateButton(Size size, Point point)
+        {
+            Button question = new Button()
+            {
+                Size = size,
+                Location = point
+            };
+            return question;
+        }
+        public Label CreateLabel(Size size, Point point)
+        {
+            Label question = new Label()
+            {
+                Size = size,
+                Location = point
+            };
+            return question;
+        }
+        public  ProgressBar CreateHealth(Point point)
+        {
+            ProgressBar health = new ProgressBar()
+            {
+                Location = point,
+                Size = new Size(190, 30),
+                Value = 100,
+                Text = "Health"
+            };
+            health.ForeColor = Color.Red;
+            return health;
+        }
+    }
+
+}
